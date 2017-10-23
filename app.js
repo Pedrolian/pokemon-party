e5f4c995159679ded55c4befe2bc4b133bf5396a
Pedrolian <pedrodvasconcelos@gmail.com>
2017-10-23 02:03:23 -0200

    Initial upload.

---
+++ app.js
@@ -0,0 +1,115 @@
+"use strict"
+
+var PokedexClass = require('./lib/pokedex/');
+var Pokedex = new PokedexClass();
+
+/*
+Pokedex.Search("pikachu", (error, data) =>
+{
+  console.log(data);
+});
+*/
+
+// requires
+const express = require('express');  
+const app = express();  
+const server = require('http').createServer(app);  
+const io = require('socket.io')(server);
+
+// Parties
+var Parties = {};
+const PartyClass = require('./lib/party');
+
+// Middleware
+app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/asset'));  
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {  
+  res.sendFile(__dirname + '/public/index.html');
+});
+app.post('/', (req, res) => 
+{
+  var party = new PartyClass(req.body.generation);
+  Parties[party.ID] = party;
+  
+  res.redirect(`/party/${party.ID}`);
+});
+
+app.get("/generation/:generationId", (req, res) => {
+  return res.send(Pokedex.Generation(req.params.generationId));
+});
+
+app.get('/party/:partyId', (req, res) => 
+{
+  if(!Parties.hasOwnProperty(req.params.partyId))
+    return res.redirect(`/`);
+  return res.sendFile(__dirname + '/public/party.html');
+});
+
+// Socket IO
+io.on('connection', (client) =>
+{  
+  console.log('Client connected...');
+
+  client.on("party", (data) => 
+  {
+    // Join room for emitting
+    client.config = { party: data };
+    client.join(`party-${data}`);
+
+    if(!Parties.hasOwnProperty(data))
+      return io.to(`party-${data}`).emit('error', "Invalid party ID.");
+
+    io.to(`party-${data}`).emit('party',Parties[data]);
+  });
+
+  client.on("input", (data) => 
+  {
+    // Process input
+    var foundError = false;
+    for(var key in data)
+    {
+      if(data[key].pokemon)
+      {
+        Pokedex.Search(data[key].pokemon, (error, pokemon) =>
+        {
+          if(error)
+          {
+            foundError = true;
+            return io.to(`party-${client.config.party}`).emit('error', `Pokemon ${data[key].pokemon} not found.`);
+          }
+          data[key].pokemon = pokemon;
+        });
+      }
+      else 
+      {
+        data[key] = false;
+      }
+    }
+
+    var counter = 1;
+    if(!foundError)
+    {
+      Parties[client.config.party].Set(data);
+      io.to(`party-${client.config.party}`).emit('party',Parties[client.config.party]);
+    }
+
+  });
+
+  /*
+  client.on('join', function(data) {
+    console.log(data);
+  });
+
+  client.on('messages', function(data) {
+    client.emit('broad', data);
+    client.broadcast.emit('broad',data);
+  });
+  */
+
+});
+
+server.listen(4200);  
